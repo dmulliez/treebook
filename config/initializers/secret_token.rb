e8c4de75fd27a2cf5831b6f77e98b5419d062954
damienmulliez <damien@l3l.fr>
2013-11-02 18:16:08 +0100

    First commit. Added statuses scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '1f6578d6453f0ffd22cbd34ee225c42586db492d40a6e111e3ff2e766db7686a2dd289f5032f08517aaec8d35c0b330863448ecde8f998b2851ffdda7ad85ad9'
