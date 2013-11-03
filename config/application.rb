0ebca5688415ebf5b35f51d293d0ea3768321a27
damienmulliez <damien@l3l.fr>
2013-11-03 16:31:15 +0100

    Install devise and try sign_up

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module Treebook
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # To use rails 3.2 and devise on heroku
+    config.assets.initialize_on_precompile = false
+
   end
 end
